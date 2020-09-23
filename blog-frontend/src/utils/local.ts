c2aa00d004bec5780686278c45773431420c7710
fff <1225109472@qq.com>
2020-09-23 23:10:23 +0800

    fix: type

--- blog-frontend/src/utils/local.ts
+++ blog-frontend/src/utils/local.ts
@@ -1,10 +1,11 @@
-export const setLocal = (key: string, val: any) => {
+
+export const setLocal: (key: string, val: any) => void = (key: string, val: any) => {
     if (typeof val === 'object') {
         val = JSON.stringify(val)
     }
     localStorage.setItem(key, val)
 }
-export const getLocal = (key: string) => {
+export const getLocal: (key: string) => string= (key: string) => {
     const body = 'Bearer '
     return body + localStorage.getItem(key)
 }
